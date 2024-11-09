c2b0a9f72ec93f943ceed356325d8f5bbd1561a7
aminhasanz <hasanzamin80@gmail.com>
2024-11-09 20:35:41 +0330

    Adding base files

---
+++ src/User.java
@@ -0,0 +1,14 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class User {
+    private final String username;
+    private final String password;
+    private String email;
+}
---
+++ src/UserServiceTest.java
@@ -0,0 +1,58 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class UserServiceTest {
+    private UserService userService;
+
+    @Before
+    public void setUp() {
+        UserRepository userRepository = new UserRepository(List.of());
+        userService = new UserService(userRepository);
+        userService.registerUser("admin", "1234");
+        userService.registerUser("ali", "qwert");
+    }
+
+    @Test
+    public void createNewValidUser__ShouldSuccess() {
+        String username = "reza";
+        String password = "123abc";
+        boolean b = userService.registerUser(username, password);
+        assertTrue(b);
+    }
+
+    @Test
+    public void createNewDuplicateUser__ShouldFail() {
+        String username = "ali";
+        String password = "123abc";
+        boolean b = userService.registerUser(username, password);
+        assertFalse(b);
+    }
+
+    @Test
+    public void loginWithValidUsernameAndPassword__ShouldSuccess() {
+        boolean login = userService.loginWithUsername("admin", "1234");
+        assertTrue(login);
+    }
+
+    @Test
+    public void loginWithValidUsernameAndInvalidPassword__ShouldFail() {
+        boolean login = userService.loginWithUsername("admin", "abcd");
+        assertFalse(login);
+    }
+
+    @Test
+    public void loginWithInvalidUsernameAndInvalidPassword__ShouldFail() {
+        boolean login = userService.loginWithUsername("ahmad", "abcd");
+        assertFalse(login);
+    }
+    @Test
+    public void loginWithValidEmailAndPassword__shouldSuccess() {
+        boolean login = userService.loginWithEmail("admin@gmail.com", "1234");
+        assertTrue(login);
+    }
+}
